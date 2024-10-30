e8a15abd1091168dd3fc93783d2455e2b99073de
KuldeepJha5176 <ranacjha@gmail.com>
2024-10-30 23:38:16 +0530

    Creating Admin Dashboard

--- src/components/Dashboards/Admin.jsx
+++ src/components/Dashboards/Admin.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Header from '../Structure/Header'
 
 const Admin = () => {
   return (
-    <div>
-      
+    <div className='h-screen w-full p-10'>
+      <Header/>
+      <div>
+       <form>
+        <h3>Task Tittle</h3>
+        <input type='text' placeholder='MAke an UI design'/>
+        <h3>Description</h3>
+        <textarea name='"' id='"' cols="30" rows="10"></textarea>
+        <h3>Date</h3>
+        <input type='date' />
+        <h3>Assign to</h3>
+        <input type='text' placeholder='employee name'/>
+        <h3>Category</h3>
+        <input type='text' placeholder='design,dev,etc'/>
+        <button>Create Task</button>
+       </form>
+      </div>
     </div>
   )
 }
