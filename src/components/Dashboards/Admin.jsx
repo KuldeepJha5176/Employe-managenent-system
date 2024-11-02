024857b19056400638e6eb93f88855180730d5d5
KuldeepJha5176 <ranacjha@gmail.com>
2024-11-02 23:33:43 +0530

    updating adminn dashboard

--- src/components/Dashboards/Admin.jsx
+++ src/components/Dashboards/Admin.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../Structure/Header'
 import Createtask from '../Structure/Createtask'
+import Alltask from '../Structure/Alltask'
 
 const Admin = () => {
   return (
     <div className='h-screen w-full p-7'>
-      <Header/>
-      <Createtask/>
+      <div >
+        <Header/>
+        <Createtask/>
+        <Alltask/>
+      </div>
+     
     </div>
   )
 }
---
+++ src/components/Structure/Alltask.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Alltask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
+      <div className='bg-red-400 py-2 px-4  flex justify-between'>
+        <h2>Kuldeep</h2>
+        <h3>Make a clean ui Design</h3>
+        <h5>Status</h5>
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default Alltask
